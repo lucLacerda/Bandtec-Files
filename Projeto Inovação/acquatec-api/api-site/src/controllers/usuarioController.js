3a90dd620858e566b54630a66ddf755e5ba66db7
lucas-bazilio <lucas.gsilva@bandtec.com.br>
2021-11-11 16:01:00 -0200

    Arquivos

--- Projeto Inovação/acquatec-api/api-site/src/controllers/usuarioController.js
+++ Projeto Inovação/acquatec-api/api-site/src/controllers/usuarioController.js
@@ -64,6 +64,7 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var cor = req.body.corServer;
 
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
@@ -73,7 +74,7 @@ function cadastrar(req, res) {
         res.status(400).send("Sua senha está undefined!");
     } else {
         
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, senha, cor)
             .then(
                 function (resultado) {
                     res.json(resultado);
