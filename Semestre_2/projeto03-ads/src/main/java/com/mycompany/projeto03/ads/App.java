10a4febf94a4d975fa0eb8eb1bcb048207f20585
Lucas Lacerda <lucas.lacerda@bandtec.com.br>
2022-02-24 19:28:52 -0300

    arquivos java

---
+++ Semestre_2/projeto03-ads/src/main/java/com/mycompany/projeto03/ads/App.java
@@ -0,0 +1,19 @@
+package com.mycompany.projeto03.ads;
+
+import com.mycompany.projeto03.ads.Utilitaria;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Utilitaria util = new Utilitaria();
+
+        System.out.println("Diego");
+        util.exibirLinha();
+        System.out.println("&".repeat(15));
+
+        System.out.println("Lucas");
+        System.out.println("0".repeat(15));
+
+    }
+}
---
+++ Semestre_2/projeto03-ads/src/main/java/com/mycompany/projeto03/ads/Utilitaria.java
@@ -0,0 +1,9 @@
+package com.mycompany.projeto03.ads;
+
+public class Utilitaria {
+
+    void exibirLinha() {
+        System.out.println("@".repeat(12));
+
+    }
+}
