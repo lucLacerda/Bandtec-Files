3a746bbf998cda8d353eefce405b68ddde182546
Lucas Lacerda <lucas.lacerda@bandtec.com.br>
2022-03-04 13:01:47 -0300

    Calculo da opcao 1

--- Semestre_2/Linguagem de Programacao/individual/src/main/java/com/mycompany/individual/Opcao1.java
+++ Semestre_2/Linguagem de Programacao/individual/src/main/java/com/mycompany/individual/Opcao1.java
@@ -11,7 +11,7 @@ public class Opcao1 {
         Double precoTotal = leitor.nextDouble();
 
         System.out.println("Informe a quantidade de parcelas que quer pagar: ");
-        Double parcelas = leitor.nextDouble();
+        Integer parcelas = leitor.nextInt();
 
         System.out.println("Tem juros? ");
         System.out.println("1 - Sim ");
@@ -19,24 +19,44 @@ public class Opcao1 {
 
         Double temJuros = leitor.nextDouble();
         Double valorJuros = 1.0;
+        Integer parcelaJuros = 0;
 
         if (temJuros == 1) {
             System.out.println("A partir de qual parcela?");
-            Double parcelaJuros = leitor.nextDouble();
+            parcelaJuros = leitor.nextInt();
 
             if (parcelas >= parcelaJuros) {
                 System.out.println("De quantos % ?");
                 valorJuros = leitor.nextDouble() / 100;
+                
+                Double calculo1 = ((precoTotal * valorJuros) + precoTotal) / parcelas;
+               
             }
         } else {
             System.out.println("Opa, que maravilha");
         }
         System.out.println("Calculando...\n");
-        Double calculo = precoTotal * valorJuros / parcelas;
+
+        Double calculo1 = (precoTotal / parcelas) * parcelaJuros;
+        Double calculo2 = precoTotal - calculo1;
+        Double calculo3 = calculo2 * parcelaJuros + precoTotal;
+
+//        Integer calculo1 = parcelas - parcelaJuros;
+//
+//        Double calculo2 = precoTotal * valorJuros * parcelaJuros / parcelaJuros;
+//
+//        Double calculo3 = precoTotal / calculo1;
+//
+//        Double calculo4 = calculo2 + calculo3;
+
         System.out.println("Analisando...\n");
         System.out.println("Finalizando...\n");
 
-        System.out.println(String.format("Resultado: %.2f", calculo));
+//        System.out.println(calculo1);
+//        System.out.println(calculo2);
+//        System.out.println(calculo3);
+
+        System.out.println(String.format("Cada parcela sairá por volta de: %.2f", calculo1));
 
     }
 
--- Semestre_2/Linguagem de Programacao/individual/src/main/java/com/mycompany/individual/ProjetoIndividual.java
+++ Semestre_2/Linguagem de Programacao/individual/src/main/java/com/mycompany/individual/ProjetoIndividual.java
@@ -7,15 +7,15 @@ public class ProjetoIndividual {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
 
-        System.out.println("Digite o numero correspondente a sua opção: ");
-        System.out.println("1 - Calcule se parcelar vale a pena");
-        System.out.println("2 - Custo X Beneficio");
-        System.out.println("4 - Encerrar Programa");
-
         Integer opcaoDigitada;
 
         do {
+            System.out.println("1 - Calcule se parcelar vale a pena");
+            System.out.println("2 - Custo X Beneficio");
+            System.out.println("4 - Encerrar Programa");
+            
             System.out.println("Digite o número correspodente a opção");
+            
             opcaoDigitada = leitor.nextInt();
 
             switch (opcaoDigitada) {
@@ -31,12 +31,12 @@ public class ProjetoIndividual {
                             + "Vamos lá: ");
                     Opcao2 funcao2 = new Opcao2();
                     funcao2.CustoBeneficio();
-                    break; 
-                    
-                case 3: 
+                    break;
+
+                case 3:
                     System.out.println("Ainda em análise");
                     break;
-                    
+
                 case 4:
                     System.out.println("Foi um prazer tê-lo conosco, volte logo!");
                     break;
@@ -44,7 +44,7 @@ public class ProjetoIndividual {
                 default:
                     System.out.println("Digite uma opção válida!");
             }
-        } while (opcaoDigitada != 1 && opcaoDigitada != 2 && opcaoDigitada != 3 && opcaoDigitada != 4);
+        } while (opcaoDigitada != 1 || opcaoDigitada != 2 || opcaoDigitada != 3 || opcaoDigitada != 4);
 
     }
 }
